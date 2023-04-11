81cb7fcd53e4ce508cb617bab94de9dc0c2655e1
ilya_stezhko@mail.ru <ilya_stezhko@mail.ru>
2023-04-11 18:43:05 +0300

    Sprint 13

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -9,7 +9,6 @@ from rest_framework.decorators import action
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-
 from reviews.models import Category, Genre, Review, Title
 from users.models import User
 
